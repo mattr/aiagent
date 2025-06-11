1ce119f3399a6860d5ad4742b3fe9c10650c0891
Matt Redmond <matt.redmond@katalyst.com.au>
2025-06-12 09:12:00 +0930

    Add verbose flag via argparse

--- main.py
+++ main.py
@@ -1,27 +1,32 @@
+import argparse
 import os
 from dotenv import load_dotenv
-import sys
+from google.genai import types
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 
 from google import genai
 
-prompt = ""
-if len(sys.argv) > 1:
-    prompt = sys.argv[1]
+parser = argparse.ArgumentParser()
+parser.add_argument("user_prompt")
+parser.add_argument("-v", "--verbose",
+                    help="Verbose: display information about the prompt and tokens",
+                    action="store_true")
 
-if len(prompt) == 0:
-    print("Please provide a prompt")
-    sys.exit(1)
+args = parser.parse_args()
 
+messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
 
 client = genai.Client(api_key=api_key)
 response = client.models.generate_content(
     model='gemini-2.0-flash-001',
-    contents=prompt
+    contents=messages
 )
+
 print(response.text)
-print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+if args.verbose:
+    print(f"User prompt: {args.user_prompt}")
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
