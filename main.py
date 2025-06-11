c729d5e4d0fb15e14580fd48e23a1eb015583e52
Matt Redmond <matt.redmond@katalyst.com.au>
2025-06-12 08:54:38 +0930

    Require a text prompt at the command line

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
 import os
 from dotenv import load_dotenv
+import sys
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 
 from google import genai
 
+prompt = ""
+if len(sys.argv) > 1:
+    prompt = sys.argv[1]
+
+if len(prompt) == 0:
+    print("Please provide a prompt")
+    sys.exit(1)
+
+
 client = genai.Client(api_key=api_key)
 response = client.models.generate_content(
     model='gemini-2.0-flash-001',
-    contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    contents=prompt
 )
 print(response.text)
 print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
